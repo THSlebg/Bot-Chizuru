a175f8c19d45b8e1c5a2e46ebb4ecb9878d7e98b
Tom <tom.lucas@viacesi.fr>
2022-09-05 20:02:34 +0200

    add commands

---
+++ commands/version.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+
+module.exports = {
+    name: "patchnote",
+    description: "Shows the patchnotes for the current version of the bot.",
+    option: null,
+    execute(interaction) {
+        return interaction.reply({
+            content: `this is version`
+        });
+    }
+}
