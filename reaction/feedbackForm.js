1845bde86d30a8f49258dfd837784c5024b45d04
hwahl <hugo.wahl@etu.univ-tours.fr>
2022-12-05 15:12:52 +0100

    Feedbacks are now sent on good channel

--- reaction/feedbackForm.js
+++ reaction/feedbackForm.js
@@ -9,28 +9,21 @@ const feedbackinfo = JSON.parse(rawdata)
 
 module.exports = {
     async execute(interaction)  {
-    const embed = new EmbedBuilder()
-            .setColor("5f5f5f")
-            .setTitle(interaction.fields.getTextInputValue('object'))
-            .setDescription(interaction.fields.getTextInputValue('description'))
-            .setThumbnail(interaction.user.avatar.url)
-            .setTimestamp();
-    
-    try {
-        const omegaguild = fs.readFileSync(path.join(datapath, "data/main_guild_info.json"));
-        const omegachannel = fs.readFileSync(path.join(datapath, "data/feedback_info.json"));
-        omegaguild.channels.get(omegachannel.feedbackChannel_id).send({embeds: [embed]})
-    } catch (error) {
-        interaction.reply({content: 'An error has occured while transfering your feedback to the devs. Try again later ...', ephemeral: true});
-        console.log(error);
-    }
-    finally {
-        interaction.reply({content: 'Message sent to developpers ! Thanks for sharing with us your opinion! ', ephemeral: true});
-    }
+        const embed = new EmbedBuilder()
+                .setColor("5f5f5f")
+                .setTitle(interaction.fields.getTextInputValue('object'))
+                .setDescription(interaction.fields.getTextInputValue('description'))
+                .setThumbnail(interaction.user.avatar.url)
+                .setTimestamp();
         
-    
-    
-    console.log(interaction.guild.channels.cache.get("957264445392949288"))
-    
+        try {
+            interaction.client.guilds.cache.get(feedbackinfo.feedbackGuild_id).channels.cache.get(feedbackinfo.feedbackChannel_id).send({embeds: [embed]})
+        } catch (error) {
+            interaction.reply({content: 'An error has occured while transfering your feedback to the devs. Try again later ...', ephemeral: true});
+            console.log(error);
+        }
+        finally {
+            interaction.reply({content: 'Message sent to developpers ! Thanks for sharing with us your opinion! ', ephemeral: true});
+        }
     }
 }
