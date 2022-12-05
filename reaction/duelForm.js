0ac0619f85833941dbfe23aa0b9f23158a229943
THSlebg <charles.legendre@viacesi.fr>
2022-12-05 15:52:58 +0100

    no conflict no noob

--- reaction/duelForm.js
+++ reaction/duelForm.js
@@ -11,6 +11,9 @@ module.exports = {
         duelinfo.nbJ = interaction.fields.getTextInputValue('nbplayer');
         duelinfo.roll = interaction.fields.getTextInputValue('roll');
         duelinfo.score = interaction.fields.getTextInputValue('score');
+
+        console.log("Duel settings : \n" + duelinfo) // pour papyK
+
     fs.writeFile(path.join(datapath, "data/server/duel_info.json"), JSON.stringify(duelinfo, null, 2), (err) => {
             if (err) {
                 console.log("Problème lors du chargement des données dans le fichier duel_info.json", err)
@@ -18,5 +21,9 @@ module.exports = {
             }
             console.log("duel_info.json  updated");
         })
+    
+    
+
+
     }
 }
